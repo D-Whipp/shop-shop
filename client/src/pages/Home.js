2e2b64fad6c5baa5d8629f3aac66afb3e806567b
Mr Whipp <dwhipp88@gmail.com>
2022-01-08 01:47:52 -0700

    cleaned up code him Home.js component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,16 +2,6 @@ import React, { useState } from "react";
 import ProductList from "../components/ProductList";
 import CategoryMenu from "../components/CategoryMenu";
 
-// const Home = () => {
-//   const [currentCategory, setCategory] = useState("");
-
-//   return (
-//     <div className="container">
-//       <CategoryMenu setCategory={setCategory} />
-//       <ProductList currentCategory={currentCategory} />
-//     </div>
-//   );
-// };
 
 const Home = () => {
   return (
