ea1c248c16ce5f2e0f17aa090ee65c1bb237d7ea
Mr Whipp <dwhipp88@gmail.com>
2022-01-14 01:15:02 -0700

    completed 22.2.6 | everything works

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -8,7 +8,15 @@ import spinner from "../assets/spinner.gif";
 // imported on 22.1.6
 // in the UPDATE PRODUCT DETAIL COMPONENT section
 import { useStoreContext } from "../utils/GlobalState";
-import { UPDATE_PRODUCTS } from "../utils/actions";
+import {
+  // Added on 1/13/22
+  // in 22.2.6
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  ADD_TO_CART,
+  // end 1/13/22 - 22.2.6
+  UPDATE_PRODUCTS,
+} from "../utils/actions";
 
 // imported on 1/12/22
 // in 22.2.4
@@ -24,6 +32,15 @@ function Detail() {
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
   const { products } = state;
+
+  // Added on 1/13/22
+  // in 22.2.6
+  const addToCart = () => {
+    dispatch({
+      type: ADD_TO_CART,
+      product: { ...currentProduct, purchaseQuantitiy: 1 },
+    });
+  };
 
   useEffect(() => {
     if (products.length) {
@@ -48,7 +65,7 @@ function Detail() {
 
           <p>
             <strong>Price:</strong>${currentProduct.price}{" "}
-            <button>Add to Cart</button>
+            <button onClick={addToCart}>Add to Cart</button>
             <button>Remove from Cart</button>
           </p>
 
